6d630252ee3161ecb1a33722b31f73c23370cbe9
= <=myjobmylife783@gmail.com>
2024-08-08 15:02:47 +0530

    updated sidebar

--- resources/views/web/includes/sidebar.blade.php
+++ resources/views/web/includes/sidebar.blade.php
@@ -7,7 +7,7 @@
         </a>
       </li> --}}
 
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="{{ route('users') }}">
           <i class="icon-grid menu-icon"></i>
           <span class="menu-title">Users</span>
